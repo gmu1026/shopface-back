dda32aa85247d480b8569dea1efc0d9be6c15844
taiya1014@naver.com <58425842yh>
2020-07-21 10:13:00 +0900

    Delete Role

--- src/main/java/com/dreamsecurity/shopface/Member/MemberServiceImpl.java
+++ src/main/java/com/dreamsecurity/shopface/Member/MemberServiceImpl.java
@@ -1,16 +1,10 @@
 package com.dreamsecurity.shopface.Member;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
