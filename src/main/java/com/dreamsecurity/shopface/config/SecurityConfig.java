23bb3517fe7dc1601c36eb0c2ae4ebbf4dba8081
gmu1026 <gmu01026@gmail.com>
2020-07-23 14:30:29 +0900

    Fix Resource Server Config

--- src/main/java/com/dreamsecurity/shopface/config/SecurityConfig.java
+++ src/main/java/com/dreamsecurity/shopface/config/SecurityConfig.java
@@ -1,18 +1,11 @@
 package com.dreamsecurity.shopface.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
--- src/main/java/com/dreamsecurity/shopface/sample/Sample.java
+++ src/main/java/com/dreamsecurity/shopface/sample/Sample.java
@@ -3,7 +3,6 @@ package com.dreamsecurity.shopface.sample;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
