48452fc568c95dbd32b64901564f1a5bf056f454
gmu1026 <gmu01026@gmail.com>
2020-07-23 17:50:15 +0900

    Fix ResourceServer OAuth Config/Fix Mapper Location

--- src/main/java/com/dreamsecurity/shopface/ShopfaceRestApplication.java
+++ src/main/java/com/dreamsecurity/shopface/ShopfaceRestApplication.java
@@ -2,7 +2,9 @@ package com.dreamsecurity.shopface;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
 @SpringBootApplication
 public class ShopfaceRestApplication {
 	public static void main(String[] args) {
--- src/main/java/com/dreamsecurity/shopface/config/SecurityConfig.java
+++ src/main/java/com/dreamsecurity/shopface/config/SecurityConfig.java
@@ -1,21 +1,16 @@
 package com.dreamsecurity.shopface.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.
@@ -26,13 +21,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
-            .authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll()
-        .and()
-            .oauth2ResourceServer().jwt();
+            .authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll();
+//        .and()
+//            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 
-    @Bean
-    JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
-    }
+//    @Bean
+//    JwtDecoder jwtDecoder() {
+//        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+//    }
 }
