3e7e91ededc704555fa7181f3fbc189af83978e6
gmu1026 <gmu01026@gmail.com>
2020-07-23 14:38:45 +0900

    Fix Resource Server Config

--- src/main/java/com/dreamsecurity/shopface/config/SecurityConfig.java
+++ src/main/java/com/dreamsecurity/shopface/config/SecurityConfig.java
@@ -1,15 +1,21 @@
 package com.dreamsecurity.shopface.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.
@@ -23,5 +29,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll()
         .and()
             .oauth2ResourceServer().jwt();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
     }
 }
